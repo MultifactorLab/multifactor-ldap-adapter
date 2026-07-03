2f8a12c9c1d625965101b5c04be544d86f02ad4b
a.andreev <a.andreev@multifactor.ru>
2026-07-03 11:04:22 +0300

    add const with length below zero guard

--- src/Server/LdapStream/LdapStreamReader.cs
+++ src/Server/LdapStream/LdapStreamReader.cs
@@ -9,6 +9,7 @@ namespace MultiFactor.Ldap.Adapter.Server.LdapStream
     public class LdapStreamReader 
     {
         private const int DEFAULT_BUFFER_SIZE = 32768;
+        private const int MAX_PACKET_SIZE = 64 * Constants.BYTES_IN_MB;
 
         private byte[] _readBuffer;
         private Stream _inputStream;
@@ -41,15 +42,16 @@ namespace MultiFactor.Ldap.Adapter.Server.LdapStream
             {
                 return GetResultPacket(_readBuffer, totalRead, false);
             }
-            //  handle multi-octate BER LEN!!
+            //  handle multi-octet BER LEN
             if (_readBuffer[1] >> 7 == 1)
             {
                 totalRead += await _inputStream.ReadAsync(_readBuffer, totalRead, _readBuffer[1] & 127);
             }
             var berLen = await Utils.BerLengthToInt(_readBuffer, 1);
             int berLenWithHeading = berLen.Length + berLen.BerByteCount + 1;
-            if(berLenWithHeading > 64 * Constants.BYTES_IN_MB)
+            if (berLen.Length < 0 || berLenWithHeading > MAX_PACKET_SIZE)
             {
+                _logger.Warning("LDAP packet of {size} byte(s) exceeds the {limit} byte(s) limit and will be bypassed without processing", berLenWithHeading, MAX_PACKET_SIZE);
                 return GetResultPacket(_readBuffer, totalRead, false);
             }
             if(berLenWithHeading >= _readBuffer.Length)
